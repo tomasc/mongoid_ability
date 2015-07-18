19f7a922874a2282929d63f193185d2c79d8eebb
Tomas Celizna <tomas.celizna@gmail.com>
2015-07-18 12:53:34 +0200

    cleanup

--- test/mongoid_ability/resolve_owner_locks_test.rb
+++ test/mongoid_ability/resolve_owner_locks_test.rb
@@ -10,24 +10,6 @@ module MongoidAbility
 
     # =====================================================================
 
-    describe 'errors' do
-      it 'raises NameError for invalid subject_type' do
-        -> { ResolveOwnerLocks.call(user, :read, 'Foo') }.must_raise NameError
-      end
-
-      it 'raises StandardError when subject_type does not have default_locks' do
-        -> { ResolveOwnerLocks.call(user, :read, Object) }.must_raise StandardError
-      end
-
-      it 'raises StandardError when subject_type class or its ancestors does not have default_lock' do
-        MySubject.stub(:default_locks, []) do
-          -> { ResolveOwnerLocks.call(user, :read, MySubject) }.must_raise StandardError
-        end
-      end
-    end
-
-    # ---------------------------------------------------------------------
-
     describe '#outcome' do
       describe 'no locks' do
         it { subject.must_be_nil }
@@ -35,26 +17,26 @@ module MongoidAbility
 
       describe 'id locks' do
         it 'returns outcome' do
-          owner.locks = [ MyLock.new(action: :read, subject: my_subject, outcome: true) ]
+          owner.my_locks = [ MyLock.new(action: :read, subject: my_subject, outcome: true) ]
           resolver_for_subject_id.must_equal true
         end
 
         it 'prefers negative outcome' do
-          owner.locks = [ MyLock.new(action: :read, subject: my_subject, outcome: true),
-                          MyLock.new(action: :read, subject: my_subject, outcome: false) ]
+          owner.my_locks = [ MyLock.new(action: :read, subject: my_subject, outcome: true),
+                             MyLock.new(action: :read, subject: my_subject, outcome: false) ]
           resolver_for_subject_id.must_equal false
         end
       end
 
       describe 'class locks' do
         it 'returns outcome' do
-          owner.locks = [ MyLock.new(action: :read, subject_type: MySubject, outcome: true) ]
+          owner.my_locks = [ MyLock.new(action: :read, subject_type: MySubject, outcome: true) ]
           subject.must_equal true
         end
 
         it 'prefers negative outcome' do
-          owner.locks = [ MyLock.new(action: :read, subject_type: MySubject, outcome: true),
-                          MyLock.new(action: :read, subject_type: MySubject, outcome: false) ]
+          owner.my_locks = [ MyLock.new(action: :read, subject_type: MySubject, outcome: true),
+                             MyLock.new(action: :read, subject_type: MySubject, outcome: false) ]
           subject.must_equal false
         end
       end
