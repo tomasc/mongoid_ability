d8d9586925f1f070e6faae594286c015df8065b3
Tomas Celizna <tomas.celizna@gmail.com>
2015-07-18 12:42:32 +0200

    cleanup

--- test/mongoid_ability/owner_test.rb
+++ test/mongoid_ability/owner_test.rb
@@ -9,36 +9,18 @@ module MongoidAbility
       let(:open_lock) { MyLock_1.new(action: :read, outcome: true, subject_type: Object.to_s) }
 
       before do
-        subject.locks = [open_lock, closed_lock].shuffle
+        subject.my_locks = [open_lock, closed_lock].shuffle
         subject.run_callbacks(:save)
       end
 
       it 'prefers closed locks' do
-        subject.locks.sort.must_equal [closed_lock].sort
+        subject.my_locks.sort.must_equal [closed_lock].sort
+      end
+
+      describe 'locks relation' do
+        it { subject.class.locks_relation_name.must_equal :my_locks }
+        it { subject.locks_relation.metadata[:name].must_equal :my_locks }
       end
     end
   end
 end
-
-
-
-# module MongoidAbility
-#   describe Owner do
-#
-#     subject { TestOwner.new }
-
-#     describe 'class methods' do
-#       describe 'lock_class_name' do
-#         it 'finds class that includes the MongoidAbility::Lock module' do
-#           TestOwner.lock_class_name.must_equal 'TestLock'
-#         end
-#       end
-#
-#       describe 'locks_relation_name' do
-#         it 'finds the name of relation that contains locks' do
-#           TestOwner.locks_relation_name.must_equal :test_locks
-#         end
-#       end
-#     end
-#   end
-# end
