fbf510c88cec02ace538e39ca4ee4773b94b6f5c
Tomas Celizna <tomas.celizna@gmail.com>
2015-07-18 14:53:30 +0200

    done, all tests pass!

--- test/mongoid_ability/can_options_test.rb
+++ test/mongoid_ability/can_options_test.rb
@@ -1,14 +1,17 @@
-# require 'test_helper'
-#
-# module MongoidAbility
-#   describe 'can options test' do
-#
-#     let(:user) { TestUser.new }
-#     let(:ability) { Ability.new(user) }
-#
-#     it 'allows to pass options to a can? block' do
-#       ability.can?(:update, TestAbilitySubjectSuper1, { foo: 'bar' })
-#     end
-#
-#   end
-# end
+require 'test_helper'
+
+module MongoidAbility
+  describe 'can options test' do
+    let(:owner) { MyOwner.new }
+    let(:ability) { Ability.new(owner) }
+
+    before do
+      MySubject.default_locks = [ MyLock1.new(action: :read, outcome: false) ]
+    end
+
+    it 'allows to pass options to a can? block' do
+      ability.can?(:read, MySubject, {}).must_equal false
+      ability.can?(:read, MySubject, { override: true }).must_equal true
+    end
+  end
+end
