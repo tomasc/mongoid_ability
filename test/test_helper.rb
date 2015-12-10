38613a72e7a2d60b021dd347fbb4cc507f63b6fc
Tomas Celizna <tomas.celizna@gmail.com>
2015-12-10 19:49:53 +0900

    disable Mongoid logging

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,6 +14,9 @@ if ENV["CI"]
   Coveralls.wear!
 end
 
+Mongoid.logger.level = Logger::INFO
+Mongo::Logger.logger.level = Logger::INFO
+
 Mongoid.configure do |config|
   config.connect_to('mongoid_ability_test')
 end
