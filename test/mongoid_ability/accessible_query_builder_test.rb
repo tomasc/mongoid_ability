0f376084756596ff79c5512b821a3dd7e35b1d84
Tomas Celizna <tomas.celizna@gmail.com>
2016-01-19 00:10:52 +0100

    wip

--- test/mongoid_ability/accessible_query_builder_test.rb
+++ test/mongoid_ability/accessible_query_builder_test.rb
@@ -1,24 +1,17 @@
-require "test_helper"
+require 'test_helper'
 
 module MongoidAbility
   describe AccessibleQueryBuilder do
-
     let(:base_class) { MySubject }
-
     let(:owner) { MyOwner.new }
     let(:ability) { Ability.new(owner) }
-
     let(:action) { :read }
+    let(:options) { Hash.new }
 
-    subject { AccessibleQueryBuilder.call(base_class, ability, action) }
-
-    before do
-      MySubject.default_locks = [ MyLock.new(subject_type: MySubject, action: :read, outcome: false) ]
-    end
+    subject { AccessibleQueryBuilder.call(base_class, ability, action, options) }
 
     it 'returns Mongoid::Criteria' do
       subject.must_be_kind_of Mongoid::Criteria
     end
-
   end
 end
--- test/mongoid_ability/subject_accessible_by_test.rb
+++ test/mongoid_ability/subject_accessible_by_test.rb
@@ -2,134 +2,138 @@ require 'test_helper'
 
 module MongoidAbility
   describe '.accessible_by' do
+    let(:my_subject) { MySubject.create! }
+    let(:my_subject1) { MySubject1.create! }
+    let(:my_subject2) { MySubject2.create! }
 
     let(:role_1) { MyRole.new }
     let(:role_2) { MyRole.new }
-    let(:owner) { MyOwner.new(my_roles: [ role_1, role_2 ]) }
+    let(:owner) { MyOwner.new(my_roles: [role_1, role_2]) }
     let(:ability) { Ability.new(owner) }
-
-    before do
-      MySubject.default_locks = [ MyLock.new(subject_type: MySubject, action: :read, outcome: true) ]
-      MySubject1.default_locks = []
-
-      @my_subject = MySubject.create!
-      @my_subject_1 = MySubject1.create!
-    end
 
     # =====================================================================
 
-    describe 'default locks' do
-      describe 'when open' do
-        it 'returns everything' do
-          MySubject.accessible_by(ability, :read).to_a.must_include @my_subject
-          MySubject.accessible_by(ability, :read).to_a.must_include @my_subject_1
-        end
+    describe 'default open locks' do
+      before do
+        # NOTE: we might need to use the .default_lock macro in case we propagate down directly
+        MySubject.default_locks = [ MyLock.new(subject_type: MySubject, action: :update, outcome: true) ]
+        MySubject1.default_locks = []
+        MySubject2.default_locks = []
+
+        my_subject
+        my_subject1
+        my_subject2
       end
 
-      describe 'when closed' do
-        before do
-          MySubject.default_locks = [ MyLock.new(subject_type: MySubject, action: :read, outcome: false) ]
-          MySubject1.default_locks = []
-        end
+      it 'propagates from superclass to all subclasses' do
+        MySubject.accessible_by(ability, :update).to_a.must_include my_subject
+        MySubject.accessible_by(ability, :update).to_a.must_include my_subject1
+        MySubject.accessible_by(ability, :update).to_a.must_include my_subject2
 
-        it 'returns nothing' do
-          MySubject.accessible_by(ability, :read).to_a.wont_include @my_subject
-          MySubject.accessible_by(ability, :read).to_a.wont_include @my_subject_1
-        end
+        MySubject1.accessible_by(ability, :update).to_a.wont_include my_subject
+        MySubject1.accessible_by(ability, :update).to_a.must_include my_subject1
+        MySubject1.accessible_by(ability, :update).to_a.must_include my_subject2
+
+        MySubject2.accessible_by(ability, :update).to_a.wont_include my_subject
+        MySubject2.accessible_by(ability, :update).to_a.wont_include my_subject1
+        MySubject2.accessible_by(ability, :update).to_a.must_include my_subject2
+      end
+    end
+
+    describe 'default closed locks' do
+      before do
+        # NOTE: we might need to use the .default_lock macro in case we propagate down directly
+        MySubject.default_locks = [ MyLock.new(subject_type: MySubject, action: :update, outcome: false) ]
+        MySubject1.default_locks = []
+        MySubject2.default_locks = []
+
+        my_subject
+        my_subject1
+        my_subject2
+      end
+
+      it 'propagates from superclass to all subclasses' do
+        MySubject.accessible_by(ability, :update).to_a.wont_include my_subject
+        MySubject.accessible_by(ability, :update).to_a.wont_include my_subject1
+        MySubject.accessible_by(ability, :update).to_a.wont_include my_subject2
+
+        MySubject1.accessible_by(ability, :update).to_a.wont_include my_subject
+        MySubject1.accessible_by(ability, :update).to_a.wont_include my_subject1
+        MySubject1.accessible_by(ability, :update).to_a.wont_include my_subject2
+
+        MySubject2.accessible_by(ability, :update).to_a.wont_include my_subject
+        MySubject2.accessible_by(ability, :update).to_a.wont_include my_subject1
+        MySubject2.accessible_by(ability, :update).to_a.wont_include my_subject2
+      end
+    end
+
+    describe 'default combined locks' do
+      before do
+        # NOTE: we might need to use the .default_lock macro in case we propagate down directly
+        MySubject.default_locks = [ MyLock.new(subject_type: MySubject, action: :update, outcome: false) ]
+        MySubject1.default_locks = [ MyLock.new(subject_type: MySubject, action: :update, outcome: true) ]
+        MySubject2.default_locks = [ MyLock.new(subject_type: MySubject, action: :update, outcome: false) ]
+
+        my_subject
+        my_subject1
+        my_subject2
+      end
+
+      it 'propagates from superclass to all subclasses' do
+        MySubject.accessible_by(ability, :update).to_a.wont_include my_subject
+        MySubject.accessible_by(ability, :update).to_a.must_include my_subject1
+        MySubject.accessible_by(ability, :update).to_a.wont_include my_subject2
+
+        MySubject1.accessible_by(ability, :update).to_a.wont_include my_subject
+        MySubject1.accessible_by(ability, :update).to_a.must_include my_subject1
+        MySubject1.accessible_by(ability, :update).to_a.wont_include my_subject2
+
+        MySubject2.accessible_by(ability, :update).to_a.wont_include my_subject
+        MySubject2.accessible_by(ability, :update).to_a.wont_include my_subject1
+        MySubject2.accessible_by(ability, :update).to_a.wont_include my_subject2
       end
     end
 
     # ---------------------------------------------------------------------
 
-    describe 'subject_type lock' do
-      describe 'on roles' do
-        it 'overrides default lock' do
-          role_1.my_locks = [ MyLock.new(subject_type: MySubject, action: :read, outcome: false) ]
-          MySubject.accessible_by(ability, :read).to_a.must_be :empty?
-        end
+    describe 'closed id locks' do
+      let(:role_1) { MyRole.new(my_locks: [ MyLock.new(subject: my_subject, action: :update, outcome: false) ]) }
 
-        it 'takes the most permissive of roles' do
-          role_1.my_locks = [ MyLock.new(subject_type: MySubject, action: :read, outcome: false) ]
-          role_2.my_locks = [ MyLock.new(subject_type: MySubject, action: :read, outcome: true) ]
-          MySubject.accessible_by(ability, :read).to_a.must_include @my_subject
-          MySubject.accessible_by(ability, :read).to_a.must_include @my_subject_1
-        end
+      before do
+        MySubject.default_locks = [ MyLock.new(subject_type: MySubject, action: :update, outcome: true) ]
+        MySubject1.default_locks = []
+        MySubject2.default_locks = []
+
+        my_subject
+        my_subject1
+        my_subject2
       end
 
-      describe 'on user' do
-        it 'overrides default lock' do
-          owner.my_locks = [ MyLock.new(subject_type: MySubject, action: :read, outcome: false) ]
-          MySubject.accessible_by(ability, :read).to_a.must_be :empty?
-        end
-
-        it 'overrides role locks' do
-          role_1.my_locks = [ MyLock.new(subject_type: MySubject, action: :read, outcome: false) ]
-          owner.my_locks = [ MyLock.new(subject_type: MySubject, action: :read, outcome: true) ]
-          MySubject.accessible_by(ability, :read).to_a.must_include @my_subject
-          MySubject.accessible_by(ability, :read).to_a.must_include @my_subject_1
-        end
+      it 'applies id locks' do
+        MySubject.accessible_by(ability, :update).to_a.wont_include my_subject
+        MySubject.accessible_by(ability, :update).to_a.must_include my_subject1
+        MySubject.accessible_by(ability, :update).to_a.must_include my_subject2
       end
     end
 
-    # ---------------------------------------------------------------------
+    describe 'open id locks' do
+      let(:role_1) { MyRole.new(my_locks: [ MyLock.new(subject: my_subject1, action: :update, outcome: true) ]) }
 
-    describe 'subject_id lock' do
-      describe 'on roles' do
-        it 'overrides default lock' do
-          role_1.my_locks = [ MyLock.new(subject: @my_subject, action: :read, outcome: false) ]
-          MySubject1.accessible_by(ability, :read).to_a.wont_include @my_subject
-        end
+      before do
+        MySubject.default_locks = [ MyLock.new(subject_type: MySubject, action: :update, outcome: false) ]
+        MySubject1.default_locks = []
+        MySubject2.default_locks = []
 
-        it 'overrides default negative lock' do
-          MySubject.default_locks = [ MyLock.new(subject_type: MySubject, action: :read, outcome: false) ]
-          role_1.my_locks = [ MyLock.new(subject: @my_subject, action: :read, outcome: true) ]
-          MySubject.accessible_by(ability, :read).to_a.must_include @my_subject
-        end
-
-        it 'overrides subject_type lock' do
-          role_1.my_locks = [ MyLock.new(subject_type: MySubject, action: :read, outcome: false) ]
-          role_2.my_locks = [ MyLock.new(subject: @my_subject, action: :read, outcome: true) ]
-          MySubject.accessible_by(ability, :read).to_a.must_include @my_subject
-        end
-
-        it 'takes the most permissive of roles' do
-          role_1.my_locks = [ MyLock.new(subject: @my_subject, action: :read, outcome: true) ]
-          role_2.my_locks = [ MyLock.new(subject: @my_subject, action: :read, outcome: false) ]
-          MySubject.accessible_by(ability, :read).to_a.must_include @my_subject
-        end
-
-        describe 'for subclasses' do
-          it 'overrides default negative lock' do
-            MySubject.default_locks = [ MyLock.new(subject_type: MySubject1, action: :read, outcome: false) ]
-            role_1.my_locks = [ MyLock.new(subject: @my_subject_1, action: :read, outcome: true) ]
-            MySubject.accessible_by(ability, :read).to_a.wont_include @my_subject
-            MySubject.accessible_by(ability, :read).to_a.must_include @my_subject_1
-          end
-        end
+        my_subject
+        my_subject1
+        my_subject2
       end
 
-      describe 'on user' do
-        it 'overrides default lock' do
-          owner.my_locks = [ MyLock.new(subject_type: MySubject, action: :read, outcome: false) ]
-          MySubject.accessible_by(ability, :read).to_a.must_be :empty?
-        end
-
-        it 'overrides subject_type lock' do
-          owner.my_locks = [
-            MyLock.new(subject_type: MySubject, action: :read, outcome: false),
-            MyLock.new(subject_type: MySubject1, action: :read, outcome: true)
-          ]
-          MySubject.accessible_by(ability, :read).to_a.wont_include @my_subject
-          MySubject.accessible_by(ability, :read).to_a.must_include @my_subject_1
-        end
-
-        it 'overrides role locks' do
-          role_1.my_locks = [ MyLock.new(subject_type: MySubject, action: :read, outcome: false) ]
-          owner.my_locks = [ MyLock.new(subject_type: MySubject, action: :read, outcome: true) ]
-          MySubject.accessible_by(ability, :read).to_a.must_include @my_subject
-        end
+      it 'applies id locks' do
+        MySubject.accessible_by(ability, :update).to_a.wont_include my_subject
+        MySubject1.accessible_by(ability, :update).to_a.must_include my_subject1
+        MySubject2.accessible_by(ability, :update).to_a.wont_include my_subject2
       end
     end
-
   end
 end
