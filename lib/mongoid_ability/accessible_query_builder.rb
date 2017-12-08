6fe335065a0d9adc13cbc5d85ba990218fedde7b
Tomáš Celizna <tomas.celizna@gmail.com>
2017-12-08 19:34:55 +0100

    remove

--- lib/mongoid_ability/accessible_query_builder.rb
+++ lib/mongoid_ability/accessible_query_builder.rb
@@ -1,64 +1,64 @@
-module MongoidAbility
-  class AccessibleQueryBuilder < Struct.new(:base_class, :ability, :action, :options)
-    def self.call(*args)
-      new(*args).call
-    end
-
-    def initialize(base_class, ability, action, options = {})
-      super(base_class, ability, action, options)
-    end
-
-    # =====================================================================
-
-    def call
-      base_class.criteria#.where(conditions)
-    end
-
-    def conditions
-      { '$and' => [{ '$or' => [closed_types_condition, open_types_and_ids_condition] }, closed_ids_condition] }
-    end
-
-    private # =============================================================
-
-    def closed_types_condition
-      { type_key => { '$nin' => values.closed_types.uniq } }
-    end
-
-    def open_types_and_ids_condition
-      {
-        type_key => { '$in' => values.open_types_and_ids.map(&:type).uniq },
-        id_key => { '$in' => values.open_types_and_ids.map(&:id).uniq }
-      }
-    end
-
-    def closed_ids_condition
-      { id_key => { '$nin' => values.closed_ids.uniq } }
-    end
-
-    # ---------------------------------------------------------------------
-
-    def values
-      @values ||= base_class.values_for_accessible_query(ability, action, options)
-    end
-
-    # ---------------------------------------------------------------------
-
-    def base_class_superclass
-      @base_class_superclass ||= (base_class.ancestors_with_default_locks.last || base_class)
-    end
-
-    # ---------------------------------------------------------------------
-
-    def prefix
-      options.fetch(:prefix, nil)
-    end
-
-    def id_key
-      [prefix, '_id'].reject(&:blank?).join.to_sym
-    end
-
-    def type_key
-      [prefix, '_type'].reject(&:blank?).join.to_sym
-    end
-  end
-end
+# module MongoidAbility
+#   class AccessibleQueryBuilder < Struct.new(:base_class, :ability, :action, :options)
+#     def self.call(*args)
+#       new(*args).call
+#     end
+#
+#     def initialize(base_class, ability, action, options = {})
+#       super(base_class, ability, action, options)
+#     end
+#
+#     # =====================================================================
+#
+#     def call
+#       base_class.criteria#.where(conditions)
+#     end
+#
+#     def conditions
+#       { '$and' => [{ '$or' => [closed_types_condition, open_types_and_ids_condition] }, closed_ids_condition] }
+#     end
+#
+#     private # =============================================================
+#
+#     def closed_types_condition
+#       { type_key => { '$nin' => values.closed_types.uniq } }
+#     end
+#
+#     def open_types_and_ids_condition
+#       {
+#         type_key => { '$in' => values.open_types_and_ids.map(&:type).uniq },
+#         id_key => { '$in' => values.open_types_and_ids.map(&:id).uniq }
+#       }
+#     end
+#
+#     def closed_ids_condition
+#       { id_key => { '$nin' => values.closed_ids.uniq } }
+#     end
+#
+#     # ---------------------------------------------------------------------
+#
+#     def values
+#       @values ||= base_class.values_for_accessible_query(ability, action, options)
+#     end
+#
+#     # ---------------------------------------------------------------------
+#
+#     def base_class_superclass
+#       @base_class_superclass ||= (base_class.ancestors_with_default_locks.last || base_class)
+#     end
+#
+#     # ---------------------------------------------------------------------
+#
+#     def prefix
+#       options.fetch(:prefix, nil)
+#     end
+#
+#     def id_key
+#       [prefix, '_id'].reject(&:blank?).join.to_sym
+#     end
+#
+#     def type_key
+#       [prefix, '_type'].reject(&:blank?).join.to_sym
+#     end
+#   end
+# end
--- lib/mongoid_ability/resolver.rb
+++ lib/mongoid_ability/resolver.rb
@@ -1,24 +1,24 @@
-module MongoidAbility
-  class Resolver < Struct.new(:owner, :action, :subject_type, :subject_id, :options)
-    attr_reader :subject_class
-
-    def self.call(*args)
-      new(*args).call
-    end
-
-    def initialize(owner, action, subject_type, subject_id = nil, options = {})
-      super(owner, action, subject_type, subject_id, options)
-
-      @subject_class = subject_type.to_s.constantize
-
-      raise StandardError, "#{subject_type} class does not have default locks" unless @subject_class.respond_to?(:default_locks)
-      raise StandardError, "#{subject_type} class does not have default lock for :#{action} action" unless @subject_class.self_and_ancestors_with_default_locks.any? do |cls|
-        cls.default_locks.any? { |l| l.action == action }
-      end
-    end
-
-    def call
-      raise NotImplementedError
-    end
-  end
-end
+# module MongoidAbility
+#   class Resolver < Struct.new(:owner, :action, :subject_type, :subject_id, :options)
+#     attr_reader :subject_class
+#
+#     def self.call(*args)
+#       new(*args).call
+#     end
+#
+#     def initialize(owner, action, subject_type, subject_id = nil, options = {})
+#       super(owner, action, subject_type, subject_id, options)
+#
+#       @subject_class = subject_type.to_s.constantize
+#
+#       raise StandardError, "#{subject_type} class does not have default locks" unless @subject_class.respond_to?(:default_locks)
+#       raise StandardError, "#{subject_type} class does not have default lock for :#{action} action" unless @subject_class.self_and_ancestors_with_default_locks.any? do |cls|
+#         cls.default_locks.any? { |l| l.action == action }
+#       end
+#     end
+#
+#     def call
+#       raise NotImplementedError
+#     end
+#   end
+# end
