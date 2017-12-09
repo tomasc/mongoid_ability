bb45224a3aa5f8a08a2bd5fde764eb7dbcb48b41
Tomáš Celizna <tomas.celizna@gmail.com>
2017-12-09 13:22:24 +0100

    all tests pass

--- test/mongoid_ability/ability_test.rb
+++ test/mongoid_ability/ability_test.rb
@@ -57,10 +57,7 @@ module MongoidAbility
     describe 'user locks' do
       describe 'when defined for superclass' do
         let(:owner) { MyOwner.new(my_locks: [MyLock.new(subject_type: MySubject, action: :read, outcome: true)]) }
-
-        before(:all) do
-          MySubject.default_lock MyLock, :read, false
-        end
+        before(:all) { MySubject.default_lock MyLock, :read, false }
 
         it { ability.can?(:read, MySubject2).must_equal true }
       end
@@ -77,71 +74,49 @@ module MongoidAbility
           ])
         end
 
-        before(:all) do
-          MySubject.default_lock MyLock, :read, false
-        end
+        before(:all) { MySubject.default_lock MyLock, :read, false }
 
         it { ability.can?(:read, MySubject).must_equal true }
       end
 
-      # describe 'when defined for superclass' do
-      #   let(:my_subject_default_locks) { [MyLock.new(subject_type: MySubject, action: :read, outcome: false)] }
-      #
-      #   before do
-      #     owner.my_roles = [MyRole.new(my_locks: [MyLock.new(subject_type: MySubject, action: :read, outcome: true)])]
-      #   end
-      #
-      #   it 'applies the superclass lock' do
-      #     MySubject.stub :default_locks, my_subject_default_locks do
-      #       MySubject1.stub :default_locks, my_subject_1_default_locks do
-      #         MySubject2.stub :default_locks, my_subject_2_default_locks do
-      #           ability.can?(:read, MySubject2).must_equal true
-      #         end
-      #       end
-      #     end
-      #   end
-      # end
+      describe 'when defined for superclass' do
+        let(:owner) do
+          MyOwner.new(my_roles: [
+            MyRole.new(my_locks: [MyLock.new(subject_type: MySubject, action: :read, outcome: true)])
+          ])
+        end
+
+        before(:all) { MySubject.default_lock MyLock, :read, false }
+
+        it { ability.can?(:read, MySubject2).must_equal true }
+      end
     end
 
     # ---------------------------------------------------------------------
 
-    # describe 'combined locks' do
-    #   describe 'user and role locks' do
-    #     let(:my_subject_default_locks) { [MyLock.new(subject_type: MySubject, action: :read, outcome: false)] }
-    #
-    #     before do
-    #       owner.my_locks = [MyLock.new(subject_type: MySubject, action: :read, outcome: false)]
-    #       owner.my_roles = [MyRole.new(my_locks: [MyLock.new(subject_type: MySubject, action: :read, outcome: true)])]
-    #     end
-    #
-    #     it 'prefers user locks' do
-    #       MySubject.stub :default_locks, my_subject_default_locks do
-    #         MySubject1.stub :default_locks, my_subject_1_default_locks do
-    #           MySubject2.stub :default_locks, my_subject_2_default_locks do
-    #             ability.can?(:read, MySubject).must_equal false
-    #           end
-    #         end
-    #       end
-    #     end
-    #   end
-    #
-    #   describe 'roles and default locks' do
-    #     let(:my_subject_default_locks) { [MyLock.new(subject_type: MySubject, action: :read, outcome: false)] }
-    #
-    #     before do
-    #       owner.my_roles = [MyRole.new(my_locks: [MyLock.new(subject_type: MySubject, action: :read, outcome: true)])]
-    #     end
-    #
-    #     it 'prefers role locks' do
-    #       MySubject.stub :default_locks, my_subject_default_locks do
-    #         MySubject1.stub :default_locks, my_subject_1_default_locks do
-    #           MySubject2.stub :default_locks, my_subject_2_default_locks do
-    #             ability.can?(:read, MySubject).must_equal true
-    #           end
-    #         end
-    #       end
-    #     end
-    #   end
-    # end
+    describe 'combined locks' do
+      describe 'user and role locks' do
+        let(:role_lock) { MyLock.new(subject_type: MySubject, action: :read, outcome: true) }
+        let(:owner_lock) { MyLock.new(subject_type: MySubject, action: :read, outcome: false) }
+
+        let(:role) { MyRole.new(my_locks: [role_lock]) }
+        let(:owner) { MyOwner.new(my_locks: [owner_lock], my_roles: [role]) }
+
+        before(:all) { MySubject.default_lock MyLock, :read, false }
+
+        it { ability.can?(:read, MySubject).must_equal false }
+      end
+
+      describe 'roles and default locks' do
+        let(:role_lock) { MyLock.new(subject_type: MySubject, action: :read, outcome: true) }
+
+        let(:role) { MyRole.new(my_locks: [role_lock]) }
+        let(:owner) { MyOwner.new(my_roles: [role]) }
+
+        before(:all) { MySubject.default_lock MyLock, :read, false }
+
+        it { ability.can?(:read, MySubject).must_equal true }
+      end
+    end
   end
 end
