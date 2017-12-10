7e79234e06dc43a6c559a83a4e61e553385e1bc5
Tomáš Celizna <tomas.celizna@gmail.com>
2017-12-10 11:30:31 +0100

    remove scoping

--- test/support/test_classes/my_lock.rb
+++ test/support/test_classes/my_lock.rb
@@ -1,12 +1,10 @@
-module MongoidAbility
-  class MyLock
-    include Mongoid::Document
-    include Mongoid::Timestamps
-    include MongoidAbility::Lock
+class MyLock
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include MongoidAbility::Lock
 
-    embedded_in :owner, polymorphic: true, touch: true
-  end
+  embedded_in :owner, polymorphic: true, touch: true
+end
 
-  class MyLock1 < MyLock
-  end
+class MyLock1 < MyLock
 end
--- test/support/test_classes/my_subject.rb
+++ test/support/test_classes/my_subject.rb
@@ -1,14 +1,12 @@
-module MongoidAbility
-  class MySubject
-    include Mongoid::Document
-    include MongoidAbility::Subject
+class MySubject
+  include Mongoid::Document
+  include MongoidAbility::Subject
 
-    field :override, type: Boolean, default: false
-  end
+  field :override, type: Boolean, default: false
+end
 
-  class MySubject1 < MySubject
-  end
+class MySubject1 < MySubject
+end
 
-  class MySubject2 < MySubject1
-  end
+class MySubject2 < MySubject1
 end
