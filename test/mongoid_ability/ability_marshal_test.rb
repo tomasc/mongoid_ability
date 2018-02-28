402d92a869b9f13e95592a1aef84ed2fc7161c83
Tomáš Celizna <tomas.celizna@gmail.com>
2018-02-28 10:20:07 +0100

    make sure marshalled ids are properly restored

--- test/mongoid_ability/ability_marshal_test.rb
+++ test/mongoid_ability/ability_marshal_test.rb
@@ -4,14 +4,23 @@ module MongoidAbility
   describe 'marshal' do
     before(:all) { MySubject.default_lock MyLock, :read, true }
 
-    let(:read_lock) { MyLock.new(subject_type: MySubject1, action: :read, outcome: false) }
+    let(:my_subject) { MySubject.new }
+    let(:read_lock) { MyLock.new(subject_type: my_subject.model_name.to_s, subject_id: my_subject.id.to_s, action: :update, outcome: false) }
     let(:owner) { MyRole.new(my_locks: [read_lock]) }
     let(:ability) { Ability.new(owner) }
 
     let(:ability_dump) { Marshal.dump(ability) }
     let(:ability_load) { Marshal.load(ability_dump) }
 
-    it { ability_dump.must_be :present? }
-    it { ability_load.send(:rules).count.must_equal 2 }
+    let(:loaded_rules) { ability_load.send(:rules) }
+
+    describe 'dump' do
+      it { ability_dump.must_be :present? }
+    end
+
+    describe 'load' do
+      it { loaded_rules.count.must_equal 2 }
+      it { loaded_rules.map(&:conditions).must_include({ id: my_subject.id }) }
+    end
   end
 end
